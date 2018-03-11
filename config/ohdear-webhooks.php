4114522192eb6a6946eca669f48124dd6034cd2e
Eric Famiglietti <eric-famiglietti@users.noreply.github.com>
2018-03-11 16:14:32 -0400

    Remove unintentional characters.
    Removed what I believe are some unintentional characters (` .`) in a comment in the config file.

--- config/ohdear-webhooks.php
+++ config/ohdear-webhooks.php
@@ -9,7 +9,7 @@ return [
     'signing_secret' => env('OH_DEAR_SIGNING_SECRET'),
 
     /*
-     * Here you can define the job that should be run when a certain webhook hits your .
+     * Here you can define the job that should be run when a certain webhook hits your
      * application.
      *
      * You can find a list of Oh dear webhook types here:
