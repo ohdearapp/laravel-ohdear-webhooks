e49abf4eb33ac90c49afc8fc6de820d204d1a7b9
freek <freek@spatie.be>
2019-02-27 20:50:39 +0100

    wip

--- tests/IntegrationTest.php
+++ tests/IntegrationTest.php
@@ -9,7 +9,7 @@ use OhDear\LaravelWebhooks\OhDearWebhookCall;
 
 class IntegrationTest extends TestCase
 {
-    public function setUp()
+    public function setUp(): void
     {
         parent::setUp();
 
